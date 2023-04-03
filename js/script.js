9c5da11a2b31cc358a5b598fee3584be570bce69
Dundaki <vigjoaopaulo@gmail.com>
2023-04-03 12:58:14 -0300

    site inicio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('funcionando')
+
+// Spinner
+var spinner = function () {
+    setTimeout(function () {
+        if ($('#spinner').length > 0) {
+            $('#spinner').removeClass('show');
+        }
+    }, 1);
+};
+spinner();
+
+
+    
